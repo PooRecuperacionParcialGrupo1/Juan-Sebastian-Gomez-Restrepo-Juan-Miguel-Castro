ea7a391ab9dc45eabfa0b3baacb990772925a612
juagomezr <juagomezr@unal.edu.co>
2026-03-23 18:41:18 -0500

    Version5
    clase pelicula y podcast terminadas

--- PPPOOGrupo1/src/main/java/Tipo2/Pelicula.java
+++ PPPOOGrupo1/src/main/java/Tipo2/Pelicula.java
@@ -8,6 +8,26 @@ package Tipo2;
  *
  * @author gomez
  */
-public class Pelicula {
+public class Pelicula extends ContenidoMultimedia {
+    String director;
     
+    public Pelicula(String titulo, int duracionMinutos, int anioLanzamiento, String director) {
+        super(titulo, duracionMinutos, anioLanzamiento);
+        this.director=director;
+    }
+    
+    @Override
+    public void iniciarReproduccion(){
+        System.out.println("Cargando video y ajustando subtitulos para la pelicula...");
+    }
+    
+    public void configurarVisualizacion(){
+        System.out.println("Reproduciendo en calidad automatica, idioma original.");
+    }
+    public void configurarVisualizacion(String artCalidad){
+        System.out.println("Reproduciendo en calidad "+ artCalidad);
+    }
+    public void configurarVisualizacion(String calidad, String idiomaSubtitulos){
+        System.out.println("Reproduciendo en calidad" + calidad + "con subtitulos en " + idiomaSubtitulos);
+    }
 }
